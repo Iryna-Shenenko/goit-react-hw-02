d80570f9777e9a0d9ae359aa53833e8b4694bf9b
Iryna Shenenko <163681074+Iryna-Shenenko@users.noreply.github.com>
2024-10-29 21:33:52 -0600

    index file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Description from "./components/Description/Description";
 import Feedback from "./components/Feedback/Feedback";
 import Options from "./components/Options/Options";
 import Notification from "./components/Notification/Notification";
 
 function App() {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [feedback, setFeedback] = useState(() => {
+    const saveFeedback = localStorage.getItem("feedback");
+    return saveFeedback
+      ? JSON.parse(saveFeedback)
+      : { good: 0, neutral: 0, bad: 0 };
   });
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
   const updateFeedback = (feedbackType) => {
     setFeedback((prevFeedback) => ({
       ...prevFeedback,
